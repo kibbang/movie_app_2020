d594c10348645fb46fea85334c441fe762109203
Shin <007kibbang@gmail.com>
2020-12-27 16:31:31 +0900

    add Routing

---
+++ src/routes/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './About.css';
+
+function About() {
+  return (
+    <div className="about__container">
+      <span>
+        React sample Clone
+      </span>
+      <span>− from NomadCoder</span>
+    </div>
+  );
+}
+export default About;
