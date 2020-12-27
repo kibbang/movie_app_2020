abe13a15165f3d3f823ad1bfda4025fc084bcc0f
Shin <007kibbang@gmail.com>
2020-12-27 11:02:07 +0900

    install axios and studying lifecycle

--- src/App.js
+++ src/App.js
@@ -1,64 +1,20 @@
 import React from 'react';
-import PropTypes from 'prop-types'; 
 
-const food = [
-  {
-    id: 1,
-    name: 'cow',
-    rating: 5
-  },
+class App extends React.Component{
+  state = {
+    isLoading: true
+  }
 
-  {
-    id: 2,
-    name: 'pig',
-    rating: 3.6
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ isLoading: false })
+    }, 6000)
+  }
 
-  },
-
-  {
-    id: 3,
-    name: 'apple',
-    rating: 2.7
-
-  },
-
-  {
-    id: 4,
-    name: 'banana',
-    rating: 4
-
-  },
-
-  {
-    id: 5,
-    name: 'fish',
-    rating: 3.9
-
-  },
-]
- 
-function Food({ name, rating }){
-  return (
-    <div>
-      <h1>I love {name}</h1>
-      <h2>{rating} / 5.0</h2>
-    </div>
-  );
-}
-
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired
-};
-
-function App() {
-  return (
-    <div>
-      {food.map(dish=>(
-        <Food key = { dish.id } name = { dish.name } rating= { dish.rating } />
-      ))}
-    </div>
-  );
+  render() {
+    const { isLoading } = this.state
+    return <div>{ isLoading ? "Loading" : "We are Ready" }</div>
+  }
 }
 
 export default App;
--- study.md
+++ study.md
@@ -6,3 +6,12 @@
     map()->array의 각 아이템에서 함수를 실행하는 array를 가지는 js함수
     그 함수의 결과를 array로 가져다 줌
     ex) food.map(arg)
+# React는 자동적으로 class Component의 render Methods를 실행 함
+    class Component를 쓰는 이유
+    state기 때문에
+    state = object 
+    데이터를 넣을 공간이 있고 데이터는 변함
+    state에 데이터를 넣음
+    문제는 데이터를 어떻게 바꾸는가
+    setState를 사용한다
+    매 순간 setState를 호출할 때마다 react는 새로운 state와 함께 render함수를 실행 함
